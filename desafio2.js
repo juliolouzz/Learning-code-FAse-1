f27ef312e339fd3c1b447aceb3d689e2cedba302
Juliolouzz <julio.cdl.vet@gmail.com>
2020-07-22 03:09:37 -0300

    ortografia corrigida e funcionando

--- desafio2.js
+++ desafio2.js
@@ -82,3 +82,4 @@ const programador = {
 
 console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`)
 
+//completo 
